82967e2908fc10f785c1ecd9ef83df5eeb54bdfc
Filippo92 <106980156+Filippo92@users.noreply.github.com>
2022-07-19 16:39:24 +0200

    19/07 16.39

---
+++ NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
@@ -0,0 +1,71 @@
+package com.example.notificationscheduler;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+public class NotificationJobService extends JobService {
+
+    //Notificatio channel id
+    private static final String PRIMARY_CHANNEL_ID="primary_notification_channel";
+
+    NotificationManager mNotifYManager;
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        createNotificationChannel();
+
+        //settiamo l'intetn che lancera l'app quando cliccata
+        PendingIntent contentPending=  PendingIntent.getActivity(this,0,new Intent(this,MainActivity.class),PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //costruisco la notifica con le sue caratteristiche
+
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,PRIMARY_CHANNEL_ID)
+                .setContentTitle("Job Service")
+                .setContentText("Your Job ran to completion!")
+                .setContentIntent(contentPending)
+                .setSmallIcon(R.drawable.ic_job_running)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true);
+
+        Notification notification=builder.build();
+
+        mNotifYManager.notify(0,notification);
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return false;
+    }
+
+   //creiamo un canale per notifiche per versione superio 8
+    public void createNotificationChannel(){
+
+        //creiamo un notificationManager
+        mNotifYManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+
+            //creiamo il notification channel con tutti i parametri
+            NotificationChannel notificationChannel=new NotificationChannel(PRIMARY_CHANNEL_ID,"job service notification",NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from job service");
+
+            mNotifYManager.createNotificationChannel(notificationChannel);
+        }
+
+    }
+}
